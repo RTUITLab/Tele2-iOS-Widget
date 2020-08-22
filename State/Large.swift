19f640767eb9723f0f52b69ae1e923e5ffad2efe
GG <ishorin@icloud.com>
2020-08-23 01:25:01 +0300

    NewAdamas

--- State/Large.swift
+++ State/Large.swift
@@ -29,7 +29,8 @@ struct Large: View {
             HStack(alignment: .center,content: {
                 VStack(alignment: .leading,content: {
                     Text("Номер")
-                        .font(.subheadline)
+                        .font(Font.custom("Tele2 DisplaySerif", size: 22))
+                        .bold()
                         .foregroundColor(.white)
                         .allowsTightening(true)
                     Text(self.entry.limits.phone)
