f849e09f15fa39c2799148682944b0ba1eec27ea
Mikhail <wowivan2011@yandex.ru>
2020-08-22 12:35:12 +0300

    Fix medium widget size

--- State/ShopAd.swift
+++ State/ShopAd.swift
@@ -13,15 +13,15 @@ struct ShopAd: View {
         HStack {
             VStack(alignment: .center){
                 VStack(alignment: .center){
-                Text ("Может быть")
-                    .font(.caption2)
-                    .foregroundColor(.white)
-                    .bold()
-                    +
-                    Text(" интересно")
-                    .font(.caption2)
-                    .foregroundColor(Color(red: 1.0, green: 0.34901960784313724, blue: 0.6392156862745098))
-                    .bold()
+                    Text ("Может быть")
+                        .font(.caption2)
+                        .foregroundColor(.white)
+                        .bold()
+                        +
+                        Text(" интересно")
+                        .font(.caption2)
+                        .foregroundColor(Color(red: 1.0, green: 0.34901960784313724, blue: 0.6392156862745098))
+                        .bold()
                     
                     Image("adamas")
                         .resizable()
@@ -29,13 +29,14 @@ struct ShopAd: View {
                         .frame(width:45, height:45)
                 }
                 .padding(.top)
+                
                 VStack(alignment: .center) {
                     Text("Бесплатная подвеска и")
                         .font(.caption2)
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
                         .padding(.top, 1.0)
-
+                    
                     Text ("скидка 15%")
                         .font(.caption2)
                         .foregroundColor(.white)
@@ -45,8 +46,10 @@ struct ShopAd: View {
                         .font(.caption2)
                         .foregroundColor(Color(red: 1.0, green: 0.34901960784313724, blue: 0.6392156862745098))
                         .bold()
+                    
                 }
                 .padding(.bottom)
+                
                 Button(action: {
                     print("Delete tapped!")
                 }) {
@@ -54,29 +57,29 @@ struct ShopAd: View {
                         Text("Активировать")
                             .padding(.vertical)
                             .font(.footnote)
-                        .foregroundColor(.black)
-                        .multilineTextAlignment(.center)
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.center)
                     }
                     .padding()
                     .foregroundColor(.white)
                     .background(Color.yellow)
                     .frame(height:25)
-
+                    
                     .cornerRadius(15)
                 }
                 .padding(.bottom, -7.0)
-                    .padding(.bottom)
-                    .padding((.all), 7.0)
-                    .frame(height: 0.0)
-                    .foregroundColor(.gray)
-                }
                 .padding(.bottom)
-                
-                  
-                    
+                .padding((.all), 7.0)
+                .frame(height: 0.0)
+                .foregroundColor(.gray)
             }
+            .padding(.bottom)
+            
+            
             
         }
+        
+    }
 }
 
 struct ShopAd_Previews: PreviewProvider {
@@ -87,6 +90,6 @@ struct ShopAd_Previews: PreviewProvider {
             .background(Color(red: 0.10980392156862745, green: 0.10980392156862745, blue: 0.11764705882352941))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
             .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
-            
+        
     }
 }
